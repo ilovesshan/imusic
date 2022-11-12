7ff14df7454fc06c106da2613a79ee1315ca3a86
wangyifeng <2665939276@qq.com>
2022-11-12 11:30:15 +0800

    feat: Repository UserRepository

---
+++ src/main/java/com/ilovesshan/imusic/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ilovesshan.imusic.repository;
+
+import com.ilovesshan.imusic.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/11/12
+ * @description:
+ */
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
