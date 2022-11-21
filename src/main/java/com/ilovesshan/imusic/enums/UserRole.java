d25f904cdd1f66f2e743e77f0c5414a52dd7c449
wangyifeng <2665939276@qq.com>
2022-11-21 11:53:30 +0800

    feat: 用户角色权限控制

---
+++ src/main/java/com/ilovesshan/imusic/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.ilovesshan.imusic.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/11/12
+ * @description:
+ */
+public enum UserRole {
+    // 管理员
+    ROLE_ADMIN,
+
+    // 普通用户
+    ROLE_USER,
+}
